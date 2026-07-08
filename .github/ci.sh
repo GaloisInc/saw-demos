f2dea2fa3e1de097eccd0f0092283656e45c186f
David Holland <dholland@galois.com>
2026-07-08 13:50:18 -0400

    Use what4-solvers instead of ad hoc downloads of z3/cvc4/yices.
    This gives us abc (which when this was last runnable was linked into
    SAW) and cvc5 instead of cvc4; however, it seems we weren't actually
    using the downloaded cvc4. Go figure.

--- .github/ci.sh
+++ .github/ci.sh
@@ -9,61 +9,6 @@ mkdir -p "$BIN"
 
 is_exe() { [[ -x "$1/$2$EXT" ]] || command -v "$2" > /dev/null 2>&1; }
 
-install_z3() {
-  is_exe "$BIN" "z3" && return
-
-  case "$RUNNER_OS" in
-    Linux) file="ubuntu-18.04.zip" ;;
-    macOS) file="osx-10.15.7.zip" ;;
-    Windows) file="win.zip" ;;
-  esac
-  curl -o z3.zip -sL "https://github.com/Z3Prover/z3/releases/download/z3-$Z3_VERSION/z3-$Z3_VERSION-x64-$file"
-
-  if $IS_WIN; then 7z x -bd z3.zip; else unzip z3.zip; fi
-  cp z3-*/bin/z3$EXT $BIN/z3$EXT
-  $IS_WIN || chmod +x $BIN/z3
-  rm z3.zip
-}
-
-install_cvc4() {
-  is_exe "$BIN" "cvc4" && return
-  version="${CVC4_VERSION#4.}" # 4.y.z -> y.z
-
-  case "$RUNNER_OS" in
-    Linux) file="x86_64-linux-opt" ;;
-    Windows) file="win64-opt.exe" ;;
-    macOS) file="macos-opt" ;;
-  esac
-  # Temporary workaround
-  if [[ "$RUNNER_OS" == "Linux" ]]; then
-    curl -o cvc4$EXT -sL "https://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/unstable/cvc4-2020-08-18-x86_64-linux-opt"
-  else
-    curl -o cvc4$EXT -sL "https://github.com/CVC4/CVC4/releases/download/$version/cvc4-$version-$file"
-  fi
-  $IS_WIN || chmod +x cvc4$EXT
-  mv cvc4$EXT "$BIN/cvc4$EXT"
-}
-
-install_yices() {
-  is_exe "$BIN" "yices" && return
-  ext=".tar.gz"
-  case "$RUNNER_OS" in
-    Linux) file="pc-linux-gnu-static-gmp.tar.gz" ;;
-    macOS) file="apple-darwin18.7.0-static-gmp.tar.gz" ;;
-    Windows) file="pc-mingw32-static-gmp.zip" && ext=".zip" ;;
-  esac
-  curl -o "yices$ext" -sL "https://yices.csl.sri.com/releases/$YICES_VERSION/yices-$YICES_VERSION-x86_64-$file"
-
-  if $IS_WIN; then
-    7z x -bd "yices$ext"
-    mv "yices-$YICES_VERSION"/bin/*.exe "$BIN"
-  else
-    tar -xzf "yices$ext"
-    (cd "yices-$YICES_VERSION" && sudo ./install-yices)
-  fi
-  rm -rf "yices$ext" "yices-$YICES_VERSION"
-}
-
 install_saw() {
   is_exe "$BIN" "saw" && return
 
@@ -90,14 +35,11 @@ install_saw() {
 }
 
 install_system_deps() {
-  install_z3 &
-  install_cvc4 &
-  install_yices &
-  install_saw &
-  wait
+  install_saw
   export PATH="$BIN:$PATH"
   echo "$BIN" >> "$GITHUB_PATH"
-  is_exe "$BIN" z3 && is_exe "$BIN" cvc4 && is_exe "$BIN" yices && is_exe "$BIN" saw && is_exe "$BIN" saw-remote-api
+  is_exe "$BIN" z3 && is_exe "$BIN" cvc5 && is_exe "$BIN" yices && is_exe "$BIN" abc
+  is_exe "$BIN" saw && is_exe "$BIN" saw-remote-api
 }
 
 output() { echo "::set-output name=$1::$2"; }
